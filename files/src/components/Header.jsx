2e8a4af339a04430f8370a65077e059b460d6a3c
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-01-06 20:49:01 +0100

    Header styling

--- files/src/components/Header.jsx
+++ files/src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div>
+        <div className='header'>
             <h1>Marek Rykaczewski</h1>
             <h3>Frontend Developer</h3>
-            <h4>website</h4>
+            <h4>marekrykaczewski.website</h4>
             <div>
                 <button>Email</button>
                 <button>LinkedIn</button>
