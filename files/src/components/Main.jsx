02031026811afb39522eb73ffa11135035da4aba
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-01-06 21:07:39 +0100

    main styling

--- files/src/components/Main.jsx
+++ files/src/components/Main.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 export default function Main() {
     return (
         <main className='main'>
+            <section>
             <h2>About</h2>
             <p> Lorem, ipsum dolor sit amet consectetur 
                 adipisicing elit. Fugit quis sint tenetur 
                 quaerat architecto mollitia aspernatur qui 
                 reiciendis odit doloremque? </p>
+            </section>
+            <section>
             <h2>Interests</h2>
             <p> Lorem ipsum dolor sit amet consectetur 
                 adipisicing elit. In, nisi? </p>
+            </section>
         </main>
     )
 }
