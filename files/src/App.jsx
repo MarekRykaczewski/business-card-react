04aa21307a5cc86f63afc6258276e0c49a526309
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-01-07 17:28:43 +0100

    remove unnecessary files and references

--- files/src/App.jsx
+++ files/src/App.jsx
@@ -1,5 +1,3 @@
-import reactLogo from './assets/react.svg'
-import './App.css'
 import Headshot from './components/Headshot.jsx'
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx'
